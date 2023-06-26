8904421cd89b3368d895d2413a0cd541eef16155
sonokai <kaibkg111@gmail.com>
2023-06-26 13:32:15 -0700

    Initial commit

---
+++ hjhmnnbn/hjhmnnbn/hjhmnnbnApp.swift
@@ -0,0 +1,17 @@
+//
+//  hjhmnnbnApp.swift
+//  hjhmnnbn
+//
+//  Created by Kai Green on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct hjhmnnbnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
